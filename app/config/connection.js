b56493001b68cd5dbd771867d940f2fd5b362c0c
willy <willygodwin47@gmail.com>
2021-02-17 13:06:40 +0800

    connectiono.js created

---
+++ app/config/connection.js
@@ -0,0 +1,22 @@
+
+const mysql = require('mysql');
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    // Your port, if not 3306
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Be sure to update with your own MySQL password!
+    password: 'Paradox911',
+    database: 'burgers_db',
+  });
+
+connection.connect();
+
+  
+module.exports = connection;
